1eefa91e660a94f5b256b6daa977a3df965acd64
aliyun9786887314 <wxy18115116156@163.com>
2025-06-05 18:42:50 +0800

    设备添加优化

--- wxy-cloud-framework/src/main/java/top/wxy/framework/security/user/UserDetail.java
+++ wxy-cloud-framework/src/main/java/top/wxy/framework/security/user/UserDetail.java
@@ -2,6 +2,7 @@ package top.wxy.framework.security.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
