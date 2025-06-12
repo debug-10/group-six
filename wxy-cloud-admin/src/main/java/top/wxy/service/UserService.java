869d22cfc258cd2c6bb09078f03edb67ec70a8cf
wuhao150313 <1840165925@qq.com>
2025-06-12 09:36:34 +0800

    useDTO

--- wxy-cloud-admin/src/main/java/top/wxy/service/UserService.java
+++ wxy-cloud-admin/src/main/java/top/wxy/service/UserService.java
@@ -3,12 +3,13 @@ package top.wxy.service;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import top.wxy.model.dto.UserDTO;
+import top.wxy.model.dto.UserUpdateDTO;
 import top.wxy.model.entity.User;
 
 public interface UserService extends IService<User> {
     UserDTO getUserById(Long id);
 
-    void updateUser(Long id, UserDTO userDTO);
+    void updateUser(Long id, UserUpdateDTO userUpdateDTO);
 
     boolean toggleUserStatus(Long id);
 
